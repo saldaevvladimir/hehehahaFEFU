d609de34f5ae3811ddf3a9ea92de9f8ad41f5b45
Nikita Makarkin <nikitamakarkin1604@gmail.com>
2023-11-18 00:15:08 +1000

    Merge with main branch and code rewriting

---
+++ tmp_design/registration/auth.js
@@ -0,0 +1,15 @@
+function chooseWay(evt, action) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(action).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
